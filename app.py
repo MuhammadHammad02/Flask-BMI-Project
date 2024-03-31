2aa839adb0aba43f407508c160ad65e244aaa619
MuhammadHammad02 <muhammadhammad_21@yahoo.com>
2024-04-01 01:03:55 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def calculate():
+    bmi = 0
+    if request.method== 'POST' and 'weight' in request.form:
+        w = float(request.form.get('weight'))
+        h = float(request.form.get('height'))
+        bmi = round(w/((h/100)** 2),2)
+    return render_template('index.html',bmi=bmi)
+
+if __name__ == '__main__':
+    app.run()
